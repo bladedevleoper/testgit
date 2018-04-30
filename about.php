1fc8f41e7d86dec5732aa9cd9ac1b9c074dfbe16
bladedevleoper <37089123+bladedevleoper@users.noreply.github.com>
2018-04-30 21:10:00 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,70 @@
+
+<?php $title = "About Syn-Code"; ?>
+<?php include 'inc/header.php'; ?>
+
+
+<div class="container"> <!-- start of div container -->
+
+	<div class="row">
+		<h3>About Syn-Code</h3>
+	</div>
+	<div class="row">
+		<p>Here at Syn-Code we pride ourselves in designing clean and responsive websites to meet the needs of our clients.</p>
+		<p>With 11 years industry experience, Syn-Code has had the pleasure of working with clients from around the world, this ranges from America, Austrailia and to the UK. We've catered for a vast range of clients in sepearate industries which range from: </p>
+	</div>
+	<div class="row">
+		<h3>Industries</h3>
+	</div>
+	<div class="row">
+		<ul>
+			<li>Finance &amp; legal</li>
+			<li>Print &amp; Design</li>
+			<li>Film &amp; photography</li>
+			<li>Healthcare</li>
+		</ul>
+	</div>
+	<div class="container">
+			<div class="row">
+		<!-- image -->
+		<div class="col-md-6 col-lg-4">
+            <div class="card">
+                <div class="card img-top">
+                    <img src="img/mock.png" alt="">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title"><a class="text-secondary" href="work.php">Mock Up</a></h3>
+                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                </div>
+            </div>
+		</div><!-- end of card 1 -->
+
+		<div class="col-md-6 col-lg-4">
+            <div class="card">
+                <div class="card img-top">
+                    <img src="img/code.png"" alt="">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title"><a class="text-secondary" href="work.php">Design</a></h3>
+                </div>
+            </div>
+		</div><!-- end of card 2 -->
+
+		<div class="col-md-6 col-lg-4">
+            <div class="card">
+                <div class="card img-top">
+                    <img src="img/Responsive.png"" alt="">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title"><a class="text-secondary" href="work.php">To Web</a></h3>
+                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                </div>
+            </div>
+		</div><!-- end of card 3 -->
+	</div>
+	</div>
+
+
+
+</div> <!-- end of container div -->
+
+<?php include 'inc/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php $title = "Syn-Code"; ?>
+<?php include "inc/header.php"; ?>
+
+
+<div class="container"><!-- main content -->
+<div class="jumbotron"><!-- Jumbotron Start -->
+
+<!-- image slider goes here -->
+
+</div> <!-- Jumbotron end -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 col-lg-4">
+            <div class="card">
+                <div class="card img-top">
+                    <img src="img/ourwork.png" alt="">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title"><a class="text-secondary" href="work.php">See Our Work</a></h3>
+                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                </div>
+            </div>
+		</div><!-- end of card 1 -->
+
+		<div class="col-md-6 col-lg-4">
+            <div class="card">
+                <div class="card img-top">
+                    <img src="img/services.png" alt="">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title"><a class="text-secondary" href="services.php">Our Services</a></h3>
+                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                </div>
+            </div>
+		</div> <!-- end of card 2 --> 
+
+		<div class="col-md-6 col-lg-4">
+            <div class="card">
+                <div class="card img-top">
+                    <img src="img/charities.png" alt="">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title"><a class="text-secondary" href="charities.php">Charities We've Helped</a></h3>
+                    <h6 class="card-subtitle mb-2 text-muted"></h6>
+                </div>
+            </div>
+		</div> <!-- end of card 3 --> 
+</div>
+</div>
+</div><!-- end of main content -->
+
+
+<?php  include "inc/footer.php"; ?>
